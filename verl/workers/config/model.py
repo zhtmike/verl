97aecfb8faafdad63511329bd5277ea2daf271bb
Mike Cheung <zhtmike@gmail.com>
2026-01-20 11:52:38 +0800

    add custom_pipeline option in verl

--- verl/workers/config/model.py
+++ verl/workers/config/model.py
@@ -139,6 +139,7 @@ class HFModelConfig(BaseConfig):
     architectures: Optional[list[str]] = None
 
     mtp: MtpConfig = field(default_factory=MtpConfig)
+    custom_pipeline: Optional[str] = None
 
     def __post_init__(self):
         import_external_libs(self.external_lib)
