e26750ed7e36d27b1ae826dbecdbf01845985e2e
Mike Cheung <zhtmike@gmail.com>
2026-02-10 16:53:17 +0800

    revert noise

--- tests/experimental/agent_loop/test_diffusion_agent_loop.py
+++ tests/experimental/agent_loop/test_diffusion_agent_loop.py
@@ -42,7 +42,6 @@ def init_config() -> DictConfig:
     config.actor_rollout_ref.rollout.agent.num_workers = 2
     config.actor_rollout_ref.rollout.skip_tokenizer_init = True
     config.actor_rollout_ref.rollout.agent.default_agent_loop = "diffusion_single_turn_agent"
-    config.actor_rollout_ref.rollout.noise_level = 1.2
     config.actor_rollout_ref.rollout.sde_window_size = 2
     config.actor_rollout_ref.rollout.sde_window_range = [0, 5]
 
