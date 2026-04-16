3fca9fb65972ba72a94eefeb489049c72dbc678f
Mike Cheung <zhtmike@gmail.com>
2026-04-16 12:25:22 +0800

    fix

--- verl/workers/config/engine.py
+++ verl/workers/config/engine.py
@@ -564,4 +564,6 @@ class TrainingWorkerConfig(BaseConfig):
     # automatically select engine and optimizer function.
     # This function takes model config and the device name as parameter.
     # Users can pass in a higher-order function to take more parameters
-    auto_select_engine_optim_fn: Callable[[HFModelConfig, str], tuple[EngineConfig, OptimizerConfig]] = None
+    auto_select_engine_optim_fn: Callable[
+        [HFModelConfig, DiffusionModelConfig, str], tuple[EngineConfig, OptimizerConfig]
+    ] = None
