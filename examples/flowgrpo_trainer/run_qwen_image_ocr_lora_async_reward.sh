f32090d04a6938369f42378465dfda2d2acf5e54
Mike Cheung <zhtmike@gmail.com>
2026-04-17 15:56:50 +0800

    add script

--- examples/flowgrpo_trainer/run_qwen_image_ocr_lora_async_reward.sh
+++ examples/flowgrpo_trainer/run_qwen_image_ocr_lora_async_reward.sh
@@ -1,4 +1,4 @@
-# QweniImage full-weight RL, vllm_omni rollout
+# Qwen-Image lora RL, vllm_omni rollout
 set -x
 
 ocr_train_path=$HOME/data/ocr/train.parquet
@@ -20,7 +20,10 @@ python3 -m verl.trainer.main_flowgrpo \
     actor_rollout_ref.model.path=$HOME/models/Qwen/Qwen-Image \
     actor_rollout_ref.model.tokenizer_path=$HOME/models/Qwen/Qwen-Image/tokenizer \
     actor_rollout_ref.model.external_lib="examples.flowgrpo_trainer.diffusers_impl" \
-    actor_rollout_ref.actor.optim.lr=3e-5 \
+    actor_rollout_ref.model.lora_rank=64 \
+    actor_rollout_ref.model.lora_alpha=128 \
+    actor_rollout_ref.model.target_modules="['to_q','to_k','to_v','to_out.0','add_q_proj','add_k_proj','add_v_proj','to_add_out','img_mlp.net.0.proj','img_mlp.net.2','txt_mlp.net.0.proj','txt_mlp.net.2']" \
+    actor_rollout_ref.actor.optim.lr=3e-4 \
     actor_rollout_ref.actor.optim.weight_decay=0.0001 \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
@@ -32,7 +35,7 @@ python3 -m verl.trainer.main_flowgrpo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.n=16 \
-    actor_rollout_ref.rollout.agent.num_workers=8 \
+    actor_rollout_ref.rollout.agent.num_workers=4 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
     actor_rollout_ref.rollout.true_cfg_scale=4.0 \
@@ -45,20 +48,26 @@ python3 -m verl.trainer.main_flowgrpo \
     actor_rollout_ref.rollout.val_kwargs.algo.noise_level=0.0 \
     actor_rollout_ref.rollout.external_lib=examples.flowgrpo_trainer.vllm_omni_impl \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
-    reward.num_workers=8 \
+    reward.num_workers=4 \
     reward.reward_manager.name=visual \
     reward.reward_model.enable=True \
     reward.reward_model.model_path=$reward_model_name \
     reward.reward_model.rollout.name=$REWARD_ENGINE \
-    reward.reward_model.rollout.tensor_model_parallel_size=4 \
+    reward.reward_model.enable_resource_pool=True \
+    reward.reward_model.nnodes=1 \
+    reward.reward_model.n_gpus_per_node=1 \
+    reward.reward_model.rollout.gpu_memory_utilization=0.9 \
+    reward.reward_model.rollout.free_cache_engine=False \
+    reward.reward_model.rollout.tensor_model_parallel_size=1 \
+    reward.reward_model.rollout.enforce_eager=False \
     reward.custom_reward_function.path=$reward_path \
     reward.custom_reward_function.name=compute_score_ocr \
     trainer.logger='["console", "wandb"]' \
     trainer.project_name=flow_grpo \
-    trainer.experiment_name=qwen_image_ocr \
+    trainer.experiment_name=qwen_image_ocr_lora_async_reward \
     trainer.log_val_generations=8 \
     trainer.val_before_train=False \
-    trainer.n_gpus_per_node=8 \
+    trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.save_freq=30 \
     trainer.test_freq=30 \
